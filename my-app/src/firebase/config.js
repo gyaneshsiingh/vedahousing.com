962488f229b364da33dcc0439f857ce1470c290c
Gyanesh singh <gyanesh9875@gmail.com>
2026-03-07 15:35:03 +0530

    Update config.js

--- my-app/src/firebase/config.js
+++ my-app/src/firebase/config.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBmDAyj13tJHiC8JmLGttt9L1UbbFBlh00",
-    authDomain: "vedahousing-v.firebaseapp.com",
-    projectId: "vedahousing-v",
-    messagingSenderId: "792515368801",
-    appId: "1:792515368801:web:21618ddc5c5bce4a9e5720",
-    measurementId: "G-9CFP8V4QML"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
