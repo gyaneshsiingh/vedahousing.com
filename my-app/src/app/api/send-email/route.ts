3dd614a6dfb90c62441a935c7c89d0ae19434ca3
Gyanesh singh <gyanesh9875@gmail.com>
2026-08-08 13:21:21 +0530

    d

--- my-app/src/app/api/send-email/route.ts
+++ my-app/src/app/api/send-email/route.ts
@@ -3,12 +3,9 @@ import { Resend } from "resend";
 
 // import { createGoogleMeet } from "@/app/services/googleCalender";
 
-const resend = new Resend(process.env.RESEND_API_KEY!);
-
-
-
 export async function POST(req: Request) {
     try {
+        const resend = new Resend(process.env.RESEND_API_KEY!);
         const { name, email, phone, date, time } = await req.json();
 
         // const meetLink = await createGoogleMeet(
